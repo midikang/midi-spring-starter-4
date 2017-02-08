278db58fc8ce28f6b59a3c16c695e978b31ee2a5
midikang <midi13@163.com>
2017-02-08 10:34:37 +0800

    add tomcat-jdbc for connection pool

---
+++ src/main/java/com/midi/spring/starter/config/DataSourceConfig.java
@@ -0,0 +1,20 @@
+package com.midi.spring.starter.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:spring/data-access.properties")
+public class DataSourceConfig {
+
+	@Autowired
+	private Environment env;
+	
+	public DataSource dataSource() {
+		return null;
+	}
+}
---
+++ src/main/resources/db/hsqldb/populateDB.sql
@@ -0,0 +1,3 @@
+INSERT INTO categories VALUES (1, 'Java', '2017-01-01', '2017-01-01');
+INSERT INTO categories VALUES (2, 'Ruby', '2017-01-02', '2017-01-02');
+INSERT INTO categories VALUES (3, 'Python', '2017-01-03', '2017-01-03');
